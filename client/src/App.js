aeeeea0bb96ba724cd1d390cb7a3dc19a4441383
JaySharma9644 <jksharma.mail9644@gmail.com>
2023-11-05 20:18:28 +0530

    export commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,180 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
+import { read, utils, writeFile } from 'xlsx';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css"
 
-function App() {
-  const [data, setData] = React.useState(null);
+const App = () => {
+  const [data, setData] = useState([]);
+  const [isLoader, setLoader] = useState(false);
+  const handleImport = ($event) => {
+    const files = $event.target.files;
+    if (files.length) {
+      const file = files[0];
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const wb = read(event.target.result);
+        const sheets = wb.SheetNames;
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+        if (sheets.length) {
+          let Data_List = [];
+          let sheet = wb.Sheets[sheets[0]];
+          //const rows = utils.sheet_to_json(sheet);
+          var range = utils.decode_range(sheet['!ref']);
+          for (var R = range.s.r; R <= range.e.r; ++R) {
+            let object = {};
+            let index = 0;
+            for (var C = range.s.c; C <= range.e.c; ++C) {
+              var cell_address = { c: C, r: R };
+              /* if an A1-style address is needed, encode the address */
+              var cell_ref = utils.encode_cell(cell_address);
+              if (sheet[cell_ref] && sheet[cell_ref] != undefined) {
+                object[index.toString()] = sheet[cell_ref].v;
+                if (sheet[cell_ref].l && sheet[cell_ref].l.Target) {
+                  sheet[cell_ref].v = sheet[cell_ref].l.Target;
+                  let link_URL = sheet[cell_ref].l.Target.trim();
+                  object["Link"] = link_URL;
+                }
+              }
+              index = index + 1;
+            }
+            if (Object.keys(object).length > 0) {
+              Data_List.push(object);
+            }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+
+          }
+          setData(Data_List)
+          //console.log("json data", Data_List);
+        }
+      }
+      reader.readAsArrayBuffer(file);
+    }
+  }
+  const makeAPICall = () => {
+    
+    try {
+      setLoader(true)
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+      };
+      fetch('http://localhost:3000/api/process', requestOptions)
+        .then(response => response.json())
+        .then(data => {
+          setLoader(false)
+          setData(data)
+
+        });
+
+
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+  const handleExport = () => {
+    console.log(data)
+    makeAPICall();
+  }
+  const downloadReport = () => {
+    const headings = [[
+      'ID',
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      'Company'
+    ]];
+    const wb = utils.book_new();
+    const ws = utils.json_to_sheet([]);
+    utils.sheet_add_aoa(ws, headings);
+    let reports = data;
+    reports.forEach(object => {
+      delete object['Link'];
+    });
+
+    utils.sheet_add_json(ws, reports, { origin: 'A2', skipHeader: true });
+    utils.book_append_sheet(wb, ws, 'Report');
+    writeFile(wb, 'Reports.xlsx');
+  }
+  const Loader = () => (
+    <div className="spinner-container">
+      <div className="loading-spinner">
+      </div>
     </div>
+  )
+  return (
+    <>
+      <div className="row mb-2 mt-5">
+        <div className="col-sm-6 offset-3">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="input-group">
+                <div className="custom-file">
+                  <input type="file" name="file" className="custom-file-input" id="inputGroupFile" required onChange={handleImport}
+                    accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" />
+                  <label className="custom-file-label" htmlFor="inputGroupFile">Choose file</label>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <button onClick={handleExport} className="btn btn-primary float-right">
+                Process<i className="fa fa-download"></i>
+              </button>
+            </div>
+            <div className="col-md-3">
+              <button onClick={downloadReport} className="btn btn-success float-right">
+                Download Report<i className="fa fa-download"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        {isLoader && <Loader/>}
+       
+      </div>
+      <div className="row">
+        <div className="col-sm-6 offset-3">
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">2</th>
+                <th scope="col">3</th>
+                <th scope="col">4</th>
+                <th scope="col">5</th>
+                <th scope="col">6</th>
+                <th scope="col">Company</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                data.length
+                  ?
+                  data.map((_item, index) => (
+                    <tr key={index}>
+                      <th scope="row">{_item[0]}</th>
+                      <td>{_item[1]}</td>
+                      <td>{_item[2]}</td>
+                      <td>{_item[3]}</td>
+                      <td>{_item[4]}</td>
+                      <td>{_item[5]}</td>
+                      <td>{_item["Transporter"] ? _item["Transporter"] : ""}</td>
+                    </tr>
+                  ))
+                  :
+                  <tr>
+                    <td colSpan="5" className="text-center">No Data Found.</td>
+                  </tr>
+              }
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+
   );
-}
+};
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,23 +1,82 @@
-const express = require("express");
-
+const express = require('express')
+const fs = require('fs');
+const got = require('got');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const path = require('path')
+const bodyparser = require('body-parser')
+const multer = require('multer')
+const xl = require('xlsx');
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const excelJs = require('exceljs');
+const cors = require('cors');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+const corsOptions = {
+  origin: '*',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+}
 
+app.use(cors(corsOptions)); // Use this after the variable declaration
+app.use(express.static('./public'));
+app.use(bodyparser.json());
+app.use(
+  bodyparser.urlencoded({
+    extended: true,
+  }),
+);
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, __dirname + '/uploads/')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now() + '-' + file.originalname)
+  },
+})
+const uploadFile = multer({ storage: storage })
+
+app.post('/api/process', async (req, res) => {
+  let list_data = req.body;
+  let promises = [];
+  if(list_data && list_data.length>0){
+    list_data.map((element) => {
+      let promise = new Promise(async (resolve, reject) => {
+        const response = await fetch(element["Link"]);
+        const body = await response.text();
+        const $ = cheerio.load(body);
+        let transporter_name = $('#lbltransporter').text();
+        element["Transporter"] =transporter_name;
+        //console.log(transporter_name);
+        resolve(transporter_name);
+      });
+      promises.push(promise);
+    });
+  
+    let result = await Promise.all(promises);
+  }else{
+    list_data =[];
+  }
+  
+  res.json(list_data);
+
+})
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
-  
+  res.json({ message: "Hello from server!" });
+});
+
 //Have Node serve the files for our built React app
 
-  app.use(express.static(path.resolve(__dirname, '../client/build')));
-  
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 //All other GET requests not handled before will return our React app
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  });
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
